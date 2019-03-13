d9538bfddeac61d86fd800fe7db02f5fbeee7bf9
Berts98 <bertolani.anna@gmail.com>
2019-03-13 18:32:30 +0100

    no message

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class Player : MonoBehaviour
+{
+    Timer time;
+    Score value;
+    public int scoreadd = 100;
+    public List<GameObject> Cubes = new List<GameObject>();
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            value.scoreValue += scoreadd;
+            Destroy(this.gameObject);
+        }
+    }
+
+    private void OnMouseDown()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            value.scoreValue += scoreadd;
+            Destroy(this.gameObject);
+        }
+    }
+
+    public void LosingConditions()
+    {
+        if (value.scoreValue <=700 && time.currentTime == 0 )
+        {
+            SceneManager.LoadScene(2);
+        }
+    }
+
+  
+    public void WinningConditions()
+    {
+        if (value.scoreValue == 700 && time.currentTime > 0)
+        {
+            SceneManager.LoadScene(3);
+        }
+    }
+}
---
+++ Assets/Scripts/Timer.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Timer : MonoBehaviour {
+
+
+    public float currentTime = 0f;
+    float startingTime = 15f;
+
+    public Text CountDown;
+
+    private void Start()
+    {
+        currentTime = startingTime;
+    }
+
+    private void Update()
+    {
+        currentTime -= 1 * Time.deltaTime;
+        CountDown.text = currentTime.ToString("0.0");
+
+        if (currentTime <= 0)
+        {
+            currentTime = 0;
+
+        }
+    }
+}
